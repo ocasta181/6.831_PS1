6375addafb61ee1fcfc0f927b420771a0da0a4df
ocasta181 <ocasta181@gmail.com>
2015-02-16 23:11:28 -0500

    Added most of style and functionality
    see changes

--- contents/translate_game.js
+++ contents/translate_game.js
@@ -1,24 +1,5 @@
 // This allows the Javascript code inside this block to only run when the page
 // has finished loading in the browser.
-/**$(function() {
-	var lang_to		= "English";
-	var lang_from		= "Spanish";
-	var current_dict	= dicts[lang_to][lang_from]; // keys: words in @lang_to, values: corresponding words in @lang_from 	
-
-	// Your code here
-
-    });
-
-
-
-    			var is_correct = (current_word == $scope.user_answer ? 1 : current_answer);
-			$scope.translation.push({"source":current_word, 
-										"translation":current_answer, 
-										"correct":is_correct});
-			random_input = Math.floor(Math.random()*current_dict.length);
-			$scope.user_answer="";
-*/
-
 
 (function(){
 
@@ -32,37 +13,87 @@
 		$scope.lang_to="English";
 		$scope.lang_from="Spanish";
 		var current_dict_json= dicts[lang_to][lang_from];
-		var current_dict = [];
+		$scope.current_dict = [];
 		for(var i in current_dict_json)
-			current_dict.push([i, current_dict_json[i]]);
+			$scope.current_dict.push([i, current_dict_json[i]]);
 
 		$scope.translations=[
 			{"source":"calabaza", "translation":"pumpkin", "correct":1},
 			{"source":"postmodernidad", "translation":"phone", "correct":"postmodernism"}
 		];
- 
-		$scope.do_submit = function(){
-			//console.log($scope.translations);
-			console.log($scope.user_answer);
+		$scope.autocomplete_dict=[];
+		$scope.random_input = Math.floor(Math.random()*($scope.current_dict.length));
+		$scope.current_word=$scope.current_dict[$scope.random_input][1];
+		$scope.current_answer=$scope.current_dict[$scope.random_input][0];
+		for(var i in $scope.current_dict)
+			$scope.autocomplete_dict.push($scope.current_dict[i][0]);
 
-			$scope.current_word == $scope.user_answer ? 1 : current_answer;
-			var is_correct = ($scope.current_word == $scope.user_answer ? 1 : current_answer);
-			
-			$scope.translations.push({"source":$scope.current_word, 
+
+		$scope.do_submit = function(){
+			$scope.current_word == $scope.user_answer ? 1 : $scope.current_answer;
+			var is_correct = ($scope.current_answer == $scope.user_answer ? 1 : $scope.current_answer);
+			console.log(is_correct);
+			console.log(" ");
+			$scope.translations.unshift({"source":$scope.current_word, 
 										"translation":$scope.user_answer, 
 										"correct":is_correct});
-			//console.log($scope.translations);
-			random_input = Math.floor(Math.random()*current_dict.length);
+
+			$scope.random_input = Math.floor(Math.random()*$scope.current_dict.length);
+
 			$scope.user_answer="";
+			$scope.current_word=$scope.current_dict[$scope.random_input][1];
+			$scope.current_answer=$scope.current_dict[$scope.random_input][0];
+			$("#input").focus();
 		};
 
-		var random_input = Math.floor(Math.random()*current_dict.length)
-		$scope.current_word=current_dict[random_input][1];
-		var current_answer=current_dict[random_input][0];
+		$scope.set_style = function(correct, row){
+			var style;
+			//console.log(correct);
+			if(row == 'source'){
+					if(correct!=1){
+						style = 'red';
+					} else {
+						style = 'blue';
+					};
+				}else if( row == 'translation'){
+					if(correct!=1){
+						style = 'red strike';
+					} else {
+						style = 'blue';
+					};
+				} else if ( row == 'correct'){
+					if(correct!=1){
+						style = 'red';
+					} else {
+						style = 'ui-icon ui-icon-check';
+					};
+			};
+			return style;
+		};
 
-		
+		$("#input").autocomplete({
+				source: function(request, response){
+					var results = $.ui.autocomplete.filter($scope.autocomplete_dict, request.term);
+					response(results.slice(0,5));
+				},
+				minLength: 2,
+				autoFocus: true,
+				delay: 500,
+				messages: {
+			        noResults: '',
+			        results: function() {}
+			    },
+			    select: function(event, ui){
+			    	if(ui.item){
+			    		$(event.target).val(ui.item.value);
+			    	}
+			    	console.log(event.target.form)
+			    	$(event.target.form).submit();
+			    }
+		});
 
 	}]);
 
 })();
+
 
