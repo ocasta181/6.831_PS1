1cfa6e889063457491442b7c8381bd0f21bdf5dc
Andrin Foster <OcastaEshu@Ocastas-MacBook-Pro.local>
2015-02-18 21:16:30 -0500

    Final version save bonus problems
    Refactored and added comments

--- contents/translate_game.js
+++ contents/translate_game.js
@@ -1,152 +1,173 @@
 // This allows the Javascript code inside this block to only run when the page
 // has finished loading in the browser.
 
+
+/*
+Translation Game app.
+
+Author: 
+	Andrin Knight Foster
+
+Collaborators: 
+	Doug G, a consultant at InterSystems. 
+	He helped debug an incompatability between Angular and jQuery in the autocomplete directive
+
+Additional Libraries: 
+	Angular.js
+*/
 (function(){
 
-	var lang_to		= "English";
-	var lang_from		= "Spanish";
-	var current_dict	= dicts[lang_to][lang_from]; // keys: words in @lang_to, values: corresponding words in @lang_from 	
+	/*
+		This is the main Angular application.
+		All functioning Angular code must exist inside of the html tag including ng-App="translateApp"
+	*/
+	angular.module('myApp', [])
 
 
-	angular.module('myApp', ['ngRoute'])
-
+	/*
+		This controller contains the majority of the functionality.
+	*/
 	.controller('myController',['$scope', function($scope){
-		$scope.lang_to="English";
-		$scope.lang_from="Spanish";
-		var current_dict_json= dicts[lang_to][lang_from];
-		$scope.current_dict = [];
-		for(var i in current_dict_json)
-			$scope.current_dict.push([i, current_dict_json[i]]);
+		$scope.lang_to="English";				//The language the user tries to translate to
+		$scope.lang_from="Spanish";				//the language the user tries to translate from
+		
 
-		$scope.translations=[
-			{"source":"calabaza", "translation":"pumpkin", "correct":1},
-			{"source":"postmodernidad", "translation":"phone", "correct":"postmodernism"}
+		// The dictionary is more easily accessed as an Array, so we create two versions:
+		var json_dict= dicts[$scope.lang_to][$scope.lang_from];		// a JSON version
+		$scope.current_dict = [];											// and an Array version
+
+		//This pulls the dictionary entries from the JSON into an Array.
+		for(var i in json_dict)
+			$scope.current_dict.push([i, json_dict[i]]);
+
+		//This is where previous answers are stored. It is pre populated with examples.
+		$scope.previous_translations=[
+			{"previous_word":"calabaza", "previous_answer":"pumpkin", "previous_correct":1},
+			{"previous_word":"postmodernidad", "previous_answer":"phone", "previous_correct":"postmodernism"}
 		];
-		$scope.autocomplete_dict=[];
-		$scope.random_input = Math.floor(Math.random()*($scope.current_dict.length));
-		$scope.current_word=$scope.current_dict[$scope.random_input][1];
-		$scope.current_answer=$scope.current_dict[$scope.random_input][0];
+
+		var random_input = Math.floor(Math.random()*($scope.current_dict.length));	//a randomly chosen index of the current_dictionary
+		$scope.current_word=$scope.current_dict[random_input][1];					// this is the word to be translated
+		var correct_answer=$scope.current_dict[random_input][0];					// this is the correct translation
+
+		autocomplete_list=[];		// This list is used to populate autocomplete suggestions. 
+									// declared in global scope for use with autocomplete directive
+		// This populates the autocomplete list with the lang_to portion of the dictionary
 		for(var i in $scope.current_dict)
-			$scope.autocomplete_dict.push($scope.current_dict[i][0]);
+			autocomplete_list.push($scope.current_dict[i][0]);
 
-
+		/*
+			This function is called when an answer is submit.
+			It takes no inputs and returns to outputs.
+		*/
 		$scope.do_submit = function(){
-			console.log("WE GOT THIS FAR!")
-			$scope.current_word == $scope.user_answer ? 1 : $scope.current_answer;
-			var is_correct = ($scope.current_answer == $scope.user_answer ? 1 : $scope.current_answer);
-			console.log($scope.current_answer);
-			console.log($scope.translations)
-			$scope.translations.unshift({"source":$scope.current_word, 
-										"translation":$scope.user_answer, 
-										"correct":is_correct});
-			console.log($scope.translations)
-			$scope.random_input = Math.floor(Math.random()*$scope.current_dict.length);
 
-			$scope.user_answer="";
-			$scope.current_word=$scope.current_dict[$scope.random_input][1];
-			$scope.current_answer=$scope.current_dict[$scope.random_input][0];
-			$("#input").focus();
+			// if the user is correct set the is correct flag to 1 otherwise set it to the correct answer
+			var is_correct = (correct_answer == $scope.user_answer ? 1 : correct_answer);
+
+			// adds to beginning of the array of stored answers.
+			$scope.previous_translations.unshift({"previous_word":$scope.current_word, 
+										"previous_answer":$scope.user_answer, 
+										"previous_correct":is_correct});
+
+			// produces a new random index of the current dictionary
+			random_input = Math.floor(Math.random()*$scope.current_dict.length);
+			// sets a new word to be translated and a new correct translation
+			$scope.current_word=$scope.current_dict[random_input][1];	
+			correct_answer=$scope.current_dict[random_input][0]; 
+
+			$scope.user_answer="";		// resets the user response box
+	
+			$("response_box").focus();		// returns focus to the response box input
 		};
 
+		//
+
+		/*
+		 	this adds style tags to the past answers depending on correctness
+		 	Params: 
+		 		correct 	- 	the value of a 'previous_correct'
+		 		row 		-	either 'previous_word', 'previous_answer', or 'previous_correct'
+
+		 	Return: 
+		 		style		-	a string of tags to be passed to the ng-class module and later used to define the css
+		*/
 		$scope.set_style = function(correct, row){
 			var style;
-			if(row == 'source'){
-					if(correct!=1){
-						style = 'red';
-					} else {
-						style = 'blue';
-					};
-				}else if( row == 'translation'){
-					if(correct!=1){
-						style = 'red strike';
-					} else {
-						style = 'blue';
-					};
-				} else if ( row == 'correct'){
-					if(correct!=1){
-						style = 'red';
-					} else {
-						style = 'ui-icon ui-icon-check';
-					};
+
+			if(row == 'previous_word'){
+				if(correct!=1){
+					style = 'red';
+				} else {
+					style = 'blue';
+				};
+
+			}else if( row == 'previous_answer'){
+				if(correct!=1){
+					style = 'red strike';
+				} else {
+					style = 'blue';
+				};
+
+			} else if ( row == 'previous_correct'){
+				if(correct!=1){
+					style = 'red';
+				} else {
+					style = 'ui-icon ui-icon-check';		// this relpaces the "1" with a jQuery ui provided checkmark 
+				};
 			};
 			return style;
 		};
-		$("#text_form").submit(function(){
-			$scope.do_submit();
-			return false;
-		});
 
 	}])
 
+	/*
+		This directive creates a new html tag type called autocomplete.
+		it allows the 
+
+	*/
 	.directive('autocomplete', ['$timeout', function($timeout){
+
+		// this includes the high level features of the directive
 		var directive = {
 			restrict: "EA",
-			link: link
+			link: link		// this points to the more detailed functionality
 
 		};
 
 		return directive;
+
+
+		/*
+			this includes the more detailed functionality
+		*/
 		function link(scope, elem, attrs){
+
+			// this overrides the default submit function of jQuery autocomplete
 			scope.submit = function(val){
-					console.log("LOOK AT ME: ",val);
 					text_form.submit = scope.do_submit();
 				};
-			var myElement = $("#autocomplete");
-			myElement.autocomplete({
-			//angular.element().find("#autocomplete").autocomplete({	
-				source: scope.autocomplete_dict,
+			var myElement = $("#response_box");
 
+			// this defines the jQuery autocomplete functionality
+			myElement.autocomplete({	
+				source: autocomplete_list,
+				minLength: 2,		// this causes autocomplete responses to only appear after two letters have been typed
+				autoFocus: true,	// this sets the focus to the first matching item in the autocomplete list
 				
+				// this causes the function to submit after a selection is made from the list by click or 'enter'
 				select: function(event, ui){
 					var self = this;
 					$timeout(function(){
 						myElement.trigger('input');
-						scope.user_answer = ui.item.value;
-			    		scope.submit(ui.item.value);
+						scope.user_answer = ui.item.value;		// this sets the answer to the autocomplete term chosen
+			    		scope.submit();
 
 					}, 0);
 				}
 			});
 		};
 	}])
-
-
-/*
-
-		$("#input").autocomplete({
-				source: function(request, response){
-					var results = $.ui.autocomplete.filter($scope.autocomplete_dict, request.term);
-					response(results.slice(0,5));
-				},
-				minLength: 2,
-				autoFocus: true,
-				messages: {
-			        noResults: '',
-			        results: function() {}
-			    },
-
-			    select: function(event, ui){
-			    		console.log("ui.item = ");
-			    		console.log(ui.item);
-			    		console.log("event.target = ");
-			    		console.log(event.target);
-			    		console.log("event.target.form = ");
-			    		console.log(event.target.form);
-			    		console.log("ui.item.value = ");
-			    		console.log(ui.item.value);
-			    		console.log("$(event.target).val(ui.item.value)= ");
-			    		console.log($(event.target).val(ui.item.value));
-			    		console.log("user_answer = ");
-			    		console.log($scope.user_answer);
-			    		//$(event.target).val(ui.item.value);
-			    		$scope.user_answer = ui.item.value;
-			    		$(event.target ).submit();
-
-			    }
-		});
-*/
-
-
 
 })();
 
